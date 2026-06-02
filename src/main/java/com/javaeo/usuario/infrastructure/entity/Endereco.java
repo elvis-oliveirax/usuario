f01748d8dfc4c0fc78298c24c108fba83ab2b493
Elvis Oliveira <elvisoliveira0100@hotmail.com>
2026-06-02 15:59:32 -0300

    refatoracao

--- src/main/java/com/javaeo/usuario/infrastructure/entity/Endereco.java
+++ src/main/java/com/javaeo/usuario/infrastructure/entity/Endereco.java
@@ -3,6 +3,8 @@ package com.javaeo.usuario.infrastructure.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.io.Serializable;
+
 @Setter
 @Getter
 @AllArgsConstructor
@@ -10,27 +12,34 @@ import lombok.*;
 @Entity
 @Table(name = "endereco")
 @Builder
-public class Endereco {
+public class Endereco implements Serializable {
 
-
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(name = "rua")
     private String rua;
+
     @Column(name = "numero")
     private Long numero;
+
     @Column(name = "complemento", length = 10)
     private String complemento;
+
     @Column(name = "cidade", length = 150)
     private String cidade;
+
     @Column(name = "estado", length = 2)
     private String estado;
+
     @Column(name = "cep", length = 9)
     private String cep;
+
 	@Column(name = "usuario_id")
-	private long usuario_id;
+	private long usuarioid;
 
 
 }
